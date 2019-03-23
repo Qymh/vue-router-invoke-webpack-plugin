0fe35640816785fa9b5c5a8aea68eae43b2a929a
Qymh <415570450@qq.com>
2019-03-23 16:49:32 +0800

    feature => add publish shell

---
+++ scripts/publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+log() {
+  echo -e "\033[31m [$(date '+%Y-%m-%d %H:%M:%S')] $1 \033[0m"
+}
+
+read -p "please write the publish version:" VERSION 
+
+read -p "Are you sure ${VERSION} will be published (y/n)" -n 1  bool
+
+echo 
+
+if [ $bool != "y" ]
+then
+  log exit
+  exit
+fi
+
+log "publish ${VERSION}"
+
+npm config set registry http://registry.npmjs.org 
+npm version $VERSION --message "feature => release ${VERSION}"
+
+git push origin master
+git push origin v$VERSION
