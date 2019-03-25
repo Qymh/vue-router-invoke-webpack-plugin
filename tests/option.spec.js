59aba26aa2ddf8184ad3bf8e8bb0a56cff8e5ad1
Qymh <415570450@qq.com>
2019-03-25 18:32:51 +0800

    feature => test

--- tests/option.spec.js
+++ tests/option.spec.js
@@ -1,94 +1,71 @@
 const webpack = require('webpack');
 const path = require('path');
 const VueRouterInvokeWebpackPlugin = require('../core');
-const alias = {
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'demos')
-    }
-  }
-};
-describe('test', () => {
+function testPlugin(options, expect) {
+  webpack({
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), 'demos')
+      }
+    },
+    plugins: [
+      new VueRouterInvokeWebpackPlugin(
+        Object.assign({ routerDir: 'tests' }, options)
+      )
+    ]
+  });
+}
+describe('option', () => {
   it('create', () => {
-    webpack({
-      ...alias,
-      plugins: [
-        new VueRouterInvokeWebpackPlugin({
-          dir: 'demos/src',
-          alias: '@/src'
-        })
-      ]
+    testPlugin({
+      dir: 'demos/src',
+      alias: '@/src'
     });
   });
 
   it('must have dir', () => {
     expect(() => {
-      webpack({
-        ...alias,
-        plugins: [new VueRouterInvokeWebpackPlugin({})]
-      });
+      testPlugin();
     }).toThrow();
   });
 
   it('must have alias', () => {
     expect(() => {
-      webpack({
-        ...alias,
-        plugins: [
-          new VueRouterInvokeWebpackPlugin({
-            dir: 'demos/src'
-          })
-        ]
-      });
+      testPlugin({ dir: 'demos/src' });
     }).toThrow();
   });
 
   it('mode should be hash or history', () => {
     expect(() => {
-      webpack({
-        ...alias,
-        plugins: [
-          new VueRouterInvokeWebpackPlugin({
-            dir: 'demos/src',
-            alias: '@/src',
-            mode: 'test'
-          })
-        ]
+      testPlugin({
+        dir: 'demos/src',
+        alias: '@/src',
+        mode: 'test'
       });
     }).toThrow();
   });
 
   it('language should be javascript or typescript', () => {
     expect(() => {
-      webpack({
-        ...alias,
-        plugins: [
-          new VueRouterInvokeWebpackPlugin({
-            dir: 'demos/src',
-            alias: '@/src',
-            language: 'test'
-          })
-        ]
+      testPlugin({
+        dir: 'demos/src',
+        alias: '@/src',
+        language: 'test'
       });
     }).toThrow();
   });
 
   it('behavior', () => {
-    webpack({
-      ...alias,
-      plugins: [
-        new VueRouterInvokeWebpackPlugin({
-          dir: 'demos/src',
-          alias: '@/src',
-          scrollBehavior: (to, from, savedPosition) => {
-            if (savedPosition) {
-              return savedPosition;
-            } else {
-              return { x: 0, y: 0 };
-            }
-          }
-        })
-      ]
+    testPlugin({
+      dir: 'demos/src',
+      alias: '@/src',
+      scrollBehavior: (to, from, savedPosition) => {
+        if (savedPosition) {
+          return savedPosition;
+        } else {
+          return { x: 0, y: 0 };
+        }
+      }
     });
   });
 });
