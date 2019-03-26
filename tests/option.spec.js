b1254896eadfa316099743678fd91e53b4bdf08f
Qymh <415570450@qq.com>
2019-03-26 22:39:53 +0800

    feature => uncomment

--- tests/option.spec.js
+++ tests/option.spec.js
@@ -30,72 +30,72 @@ function testPlugin(options, expectVal, notExpectVal) {
   }
 }
 describe('option', () => {
-  // it('create', () => {
-  //   testPlugin({
-  //     dir: 'tests/single',
-  //     alias: '@/single'
-  //   });
-  // });
+  it('create', () => {
+    testPlugin({
+      dir: 'tests/single',
+      alias: '@/single'
+    });
+  });
 
-  // it('must have dir', () => {
-  //   expect(() => {
-  //     testPlugin();
-  //   }).toThrow();
-  // });
+  it('must have dir', () => {
+    expect(() => {
+      testPlugin();
+    }).toThrow();
+  });
 
-  // it('must have alias', () => {
-  //   expect(() => {
-  //     testPlugin({ dir: 'tests/single' });
-  //   }).toThrow();
-  // });
+  it('must have alias', () => {
+    expect(() => {
+      testPlugin({ dir: 'tests/single' });
+    }).toThrow();
+  });
 
-  // it('mode should be hash or history', () => {
-  //   expect(() => {
-  //     testPlugin({
-  //       dir: 'tests/single',
-  //       alias: '@/single',
-  //       mode: 'test'
-  //     });
-  //   }).toThrow();
-  // });
+  it('mode should be hash or history', () => {
+    expect(() => {
+      testPlugin({
+        dir: 'tests/single',
+        alias: '@/single',
+        mode: 'test'
+      });
+    }).toThrow();
+  });
 
-  // it('language should be javascript or typescript', () => {
-  //   expect(() => {
-  //     testPlugin({
-  //       dir: 'tests/single',
-  //       alias: '@/single',
-  //       language: 'test'
-  //     });
-  //   }).toThrow();
-  // });
+  it('language should be javascript or typescript', () => {
+    expect(() => {
+      testPlugin({
+        dir: 'tests/single',
+        alias: '@/single',
+        language: 'test'
+      });
+    }).toThrow();
+  });
 
-  // it('typescript', () => {
-  //   testPlugin(
-  //     {
-  //       dir: 'tests/single',
-  //       alias: '@/single',
-  //       language: 'typescript'
-  //     },
-  //     '{RouteConfig}'
-  //   );
-  // });
+  it('typescript', () => {
+    testPlugin(
+      {
+        dir: 'tests/single',
+        alias: '@/single',
+        language: 'typescript'
+      },
+      '{RouteConfig}'
+    );
+  });
 
-  // it('behavior', () => {
-  //   testPlugin(
-  //     {
-  //       dir: 'tests/single',
-  //       alias: '@/single',
-  //       scrollBehavior: (to, from, savedPosition) => {
-  //         if (savedPosition) {
-  //           return savedPosition;
-  //         } else {
-  //           return { x: 0, y: 0 };
-  //         }
-  //       }
-  //     },
-  //     'scrollBehavior:\\(to,from,savedPosition\\)'
-  //   );
-  // });
+  it('behavior', () => {
+    testPlugin(
+      {
+        dir: 'tests/single',
+        alias: '@/single',
+        scrollBehavior: (to, from, savedPosition) => {
+          if (savedPosition) {
+            return savedPosition;
+          } else {
+            return { x: 0, y: 0 };
+          }
+        }
+      },
+      'scrollBehavior:\\(to,from,savedPosition\\)'
+    );
+  });
 
   it('ignore', () => {
     testPlugin(
