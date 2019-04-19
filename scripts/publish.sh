8d4ec05d512389235ac99ccccf59be9d63546337
Qymh <415570450@qq.com>
2019-04-20 02:06:08 +0800

    feature => branch check

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,14 @@ log() {
   echo -e "\033[31m [$(date '+%Y-%m-%d %H:%M:%S')] $1 \033[0m"
 }
 
+branch=`sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' -e 's/\//\_/g'`
+
+if [ $branch != "master"]
+then
+  log only master branch can publish code
+  exit
+fi
+
 read -p "please write the publish version:" VERSION 
 
 read -p "Are you sure ${VERSION} will be published (y/n)" -n 1  bool
